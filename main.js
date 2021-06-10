ae6efe314677c041bd2dc3ec973b8c2550dc9136
Bruno <bru_16@hotmail.com.ar>
2021-06-10 18:19:59 -0300

    changes

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.querySelector('.nav-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
-})
+});
+
+ScrollReveal().reveal('.showcase', { delay: 400 });
+ScrollReveal().reveal('.cards', { delay: 1000 });
+ScrollReveal().reveal('.devicons', { delay: 700 });
